0b2554e9652c86b8965417368fe98c97b91c057d
julian rodriguez <julian.9911@hotmail.com>
2025-10-24 10:33:08 -0500

    correccion astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from "@astrojs/vercel/server";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   output: "server", //  Necesario porque ahora usas Supabase (SSR)
