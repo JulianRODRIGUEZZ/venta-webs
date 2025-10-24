3e3d99ea2440ceae67bea4ba393d340421e7f148
julian rodriguez <julian.9911@hotmail.com>
2025-10-24 10:29:50 -0500

    update vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
-// 👇 Importa el adaptador de Vercel
 import vercel from "@astrojs/vercel/server";
 
 export default defineConfig({
-  output: "server", // 👈 Necesario porque ahora usas Supabase (SSR)
-  adapter: vercel(), // 👈 Indica a Astro que use Vercel como runtime
+  output: "server", //  Necesario porque ahora usas Supabase (SSR)
+  adapter: vercel(), //  Indica a Astro que use Vercel como runtime
   integrations: [tailwind(), react()],
 });
